628cb695a8db51e8725f9f8eeee639803e68f3ce
Oskar Werkelin Ahlin <osk@spotify.com>
2013-02-20 14:52:52 +0000

    Added GameMaster sketch and an example package.

--- game_master.py
+++ game_master.py
@@ -1,7 +1,19 @@
+import json
+import subprocess
+import tarfile
+
+# directory in which to unpack packages
+# (should be a separate partition and properly sandboxed)
+DIR_PREFIX = "tmp"
+
 class GameMaster(object):
 
     """
-    Creates a new GameMaster with the given players.
+    Creates a new GameMaster with two strings, one path
+    to each .tar object. 
+
+    The .tar should contain a manifest.json in the root
+    directory.
     """
     def __init__(self, player1, player2):
         self.player1 = player1 
@@ -12,13 +24,23 @@ class GameMaster(object):
     unpacks, spawns to subprocesses and plays a game 
     between them. Then returns the result of the game, or
     an error in case one of the players failed.
+
+    Note: This is completely insecure atm.
     """
     def playMatch(self):
-        # TODO:
-        # fetch player data
-        # unpack
-        # spawn the processes, use stdio to communicate
-        # kill processes when game is over
-        # return match result
-        return
-        
+        p1_tar = tarfile.open(self.player1)
+        p2_tar = tarfile.open(self.player2)
+        p1_tar.extractall(DIR_PREFIX + "/1")
+        p2_tar.extractall(DIR_PREFIX + "/2")
+        paths = [DIR_PREFIX+"/1/manifest.json",DIR_PREFIX+"/2/manifest.json"]
+        files = []
+        # open the config files
+        for p in paths:
+            files.append(open(p))
+        # JSON parse them
+        configs = map(json.load,files)
+        # and close them
+        for f in files:
+            f.close()
+
+        # TODO: Spawn threads and make them talk.
