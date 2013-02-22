154b118e8b7babb0e0c3bc31970919ea36438a4a
Javier Ubillos <jav@spotify.com>
2013-02-22 10:26:06 +0000

    Getting a match from the coordinator now works. The match-list does not.

--- challenge_queue.py
+++ challenge_queue.py
@@ -1,4 +1,4 @@
-from random import random
+from random import randrange
 from sqlalchemy import Column, Integer, String, Boolean
 from sqlalchemy import Enum, MetaData, ForeignKey, Table
 from sqlalchemy.orm import relationship
@@ -37,24 +37,35 @@ class ChallengeQueue(Base):
         pass
 
     def get_match(self, nr_players):
-        players = self._random_select_set(nr_players, db_session.query(Player))
+        players = self._random_select_player_set(nr_players, db_session)
         print "players: %s" % (players,)
         return Match(state='unplaid', players=players)
 
     def __len__(self):
         return len(self.queue)
 
-
     def _update_players(self):
         pass
 
-    def _random_select_set(self, count, query):
-        row_count = int(query.count())
+    def _random_select_player_set(self, count, session):
+        query = session.query(Player)
+        enabled_row_count =  int(query.filter_by(enabled=True).count()) # We are using the (broken) assumption that rowcount >= max(id)
+                                                               # and we are not checking for unique owners.
 
+        if count > enabled_row_count:
+            raise IndexError("Not enough players available for a full match")
+
+        row_count = int(query.count()) # We are using the (broken) assumption that rowcount >= max(id)
+        print "count:%s, row_count:%s"%(count, row_count)
         result_set = set()
         while len(result_set) < count:
-            obj =  query.offset(int(row_count*random())).first()
-            print "result_set.add(%s), len(result_set): %s"%(obj,len(result_set))
+#            obj =  query.filter_by(id=int(row_count*random())).first()
+            id = randrange(row_count+1)
+            print "randrange(%s): %s"%(row_count, id)
+            obj = query.filter_by(id=id).filter_by(enabled=True).first()
+            print "result_set.add(%s), len(result_set): %s: result_set: %s "%(obj,len(result_set), result_set)
+            if obj is None:
+                continue # This dosen't feel right, but should work
             result_set.add(obj)
 
         print "_random_select_set(%s, %s) returns: %s"%(count, query, result_set)
--- player.py
+++ player.py
@@ -18,15 +18,10 @@ class Player(Base):
         self.owner = kwargs['owner']
         self.desc = kwargs.get('desc', '')
         self.files = kwargs.get('files', None)
+        self.enabled = True
 
     def __repr__(self):
         return "<Player(owner:'%s', desc:'%s', files: %s)>" % (self.owner, self.desc, self.files)
-
-    def add_files_uri(self, files):
-        self.files = files
-
-    def get_files_uri(self):
-        return self.files
 
     def serialize(self):
         '''Return object data in easily serializeable format'''
