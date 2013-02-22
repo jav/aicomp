6657e50011fb76b3bb0f116f39f748cd2de3e22d
Javier Ubillos <jav@spotify.com>
2013-02-22 14:16:57 +0000

    Dirty commit, time to pickup (my own) kids\!

--- game_master.py
+++ game_master.py
@@ -1,8 +1,10 @@
 import json
 import os
+from random import randrange
 import subprocess
-import tarfile
 import sys
+import tarfile
+import tempfile
 import time
 import urllib2
 
@@ -16,21 +18,23 @@ class ProcessHandler():
     last_line = ""
 
     def __init__(self, args):
+        print "Spawning process: %s" %(args,)
         self.process = subprocess.Popen(args, stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
 
     def communicate(self,input_list):
         # TODO: blocking?
         for data in input_list:
+            print "communicate(%s): data:%s in input_list: %s"%(input_list, data, input_list)
             self.process.stdin.write(data)
+            print "communicate(%s): process: %s"%(input_list, self.process)
+            self.process.stdin.flush()
 
-    def readline(self): 
+    def readline(self):
         def readline_threaded():
             self.last_line = self.process.stdout.readline()
         t = Thread(target=readline_threaded)
         t.start()
-        t.join(0.5) 
-        if t.isAlive():
-            print "ERROR"
+        t.join(1)
         return self.last_line
 
 
@@ -49,28 +53,57 @@ class GameMaster(object):
     def run(self):
         while True:
             print "checking for data ..."
-            
+
             # Check if the coordinator has any jobs for us, if so play a game,
             # and report the result back to the coordinator.
-            
+
             # REST API for fetching game information
-            req = urllib2.Request("http://ws.spotify.com/search/1/track.json?q=kaizers+orchestra")
+            req = urllib2.Request("http://localhost:5000/match/get/2")
+
             opener = urllib2.build_opener()
             f = opener.open(req)
             jsonz = json.load(f)
-            
+
             print jsonz
 
             # check json file for paths, call setUp with player paths, then do
             # playMatch
 
+            print jsonz
+
+            try:
+                urls = ["http://localhost:5000/"+x['files'] for x in jsonz['players']]
+            except:
+                continue # UGLY SHIIIT!
+            self.setUp(urls[0], urls[1])
+            self.reportWinner(self.playMatch())
+
             time.sleep(1)
         return
 
-    def setUp(self, player1, player2):
-        self.player1 = player1 
-        self.player2 = player2
-    
+    def setUp(self, player1_url, player2_url):
+        (self.player1_fh, self.player1) = tempfile.mkstemp()
+        (self.player2_fh, self.player2) = tempfile.mkstemp()
+
+        print "Opening url: %s" % (player1_url,)
+        req = urllib2.urlopen(player1_url)
+        CHUNK = 16 * 1024
+        with open(self.player1, 'wb') as fp:
+            while True:
+                chunk = req.read(CHUNK)
+                if not chunk: break
+                fp.write(chunk)
+
+        print "Opening url: %s" % (player2_url,)
+        req = urllib2.urlopen(player2_url)
+        CHUNK = 16 * 1024
+        with open(self.player2, 'wb') as fp:
+            while True:
+                chunk = req.read(CHUNK)
+                if not chunk: break
+                fp.write(chunk)
+
+
         p1_tar = tarfile.open(self.player1)
         p2_tar = tarfile.open(self.player2)
         p1_tar.extractall(DIR_PREFIX + "/1")
@@ -90,18 +123,51 @@ class GameMaster(object):
         # make the players talk!
         player1_bin_args = ["python",os.getcwd()+"/"+DIR_PREFIX+"/1/" + configs[0]['executable']]
         player2_bin_args = ["python",os.getcwd()+"/"+DIR_PREFIX+"/2/" + configs[1]['executable']]
-        
+
         self.p1 = ProcessHandler(player1_bin_args)
         self.p2 = ProcessHandler(player2_bin_args)
 
-    def playMatch(self):
-        print "PLAYING GAME!"
-        # p1 thinks about a number
-        self.p1.communicate("think\n")
-        number = int(self.p1.readline())
-        
-        # p2 tries to guess the number
-        self.p2.communicate("guess\n")
-        guessed = int(self.p2.readline())
 
-        print "thought about " + str(number) + ", guessed " + str(guessed)
+
+    def playMatch(self):
+        secret_number=randrange(1,10)
+        print "PLAYING GAME: Guess %d"%(secret_number,)
+
+        turns = 100
+        for turn in range(turns):
+            # p1 gets to guess
+            guess = -1
+            self.p1.communicate(["guess\n"])
+            try:
+                guess = int(self.p1.readline())
+                print 'Player one: "I guess: %d."'%(guess,)
+            except Exception as ex:
+                print 'Player one: "I pass.", %s'%(ex,)
+                pass
+            if guess == secret_number:
+                return [1,0]
+            # p2 tries to guess the number
+            self.p2.communicate(["guess\n"])
+            try:
+                guess = int(self.p2.readline())
+                print 'Player two: "I guess: %d."'%(guess,)
+            except:
+                print 'Player two: "I pass.", %s'%(ex,)
+                pass
+            if guess == secret_number:
+                return [0,1]
+
+        return [0,0] # Tied game
+
+    def reportWinner(self):
+        #This should call the coordinator and let it know what the result was
+        pass
+
+if __name__ == "__main__":
+
+    url = "http://localhost:5000/match/get/2" # should be sys.argv[1]
+    gm = GameMaster(url)
+    gm.run()
+
+
+    
--- static/uploaded_files/README.md
+++
@@ -1,1 +0,0 @@
-This directory is for uploaded files (players). It's name is specified in webfront.conf.
