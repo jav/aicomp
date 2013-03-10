c1d2ff696428042c644a741114cad7ad35dda2c4
Javier Ubillos <jav@spotify.com>
2013-03-10 00:04:23 +0000

    Added association-proxy for playerresults in match.

--- database.py
+++ database.py
@@ -1,8 +1,17 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import scoped_session, sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.engine import reflection
+from sqlalchemy.schema import (
+    MetaData,
+    Table,
+    DropTable,
+    ForeignKeyConstraint,
+    DropConstraint,
+    )
 
-engine = create_engine('sqlite:////tmp/test.db', convert_unicode=True)
+sqlite_db_file = "/tmp/test.db"
+engine = create_engine('sqlite://%s'%(sqlite_db_file,), convert_unicode=True)
 db_session = scoped_session(sessionmaker(autocommit=False,
                                          autoflush=False,
                                          bind=engine))
@@ -11,3 +20,6 @@ Base.query = db_session.query_property()
 
 def init_db(**kwargs):
     Base.metadata.create_all(bind=engine)
+
+def reset_db():
+    Base.metadata.drop_all(bind=engine)
